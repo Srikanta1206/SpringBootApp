3a44908e79aece2acd65f5d835570eff83d1fd41
Lenovo <Lenovo@DESKTOP-2P52UUT>
2023-09-30 20:28:25 +0530

    done by srikanta

---
+++ Boot-JPA-InteractingWithMultipleDataBase/src/main/java/com/sri/BootJpaInteractingWithMultipleDataBaseApplication.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJpaInteractingWithMultipleDataBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJpaInteractingWithMultipleDataBaseApplication.class, args);
+	}
+
+}
