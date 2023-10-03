ab3f9e97a488803177168c0fdb30b6324eddbadf
Lenovo <Lenovo@DESKTOP-2P52UUT>
2023-10-03 19:28:49 +0530

    Projct created

---
+++ Boot-Batch-Proj-1/src/main/java/com/sri/BootBatchProj1Application.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootBatchProj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootBatchProj1Application.class, args);
+	}
+
+}
