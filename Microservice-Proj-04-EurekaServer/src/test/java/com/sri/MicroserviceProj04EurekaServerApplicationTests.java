b5d03f3e7f0a91b0a982cf9aa87d28a74c6f2406
Lenovo <Lenovo@DESKTOP-2P52UUT>
2023-11-10 22:40:28 +0530

    working with config server

---
+++ Microservice-Proj-04-EurekaServer/src/test/java/com/sri/MicroserviceProj04EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceProj04EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
